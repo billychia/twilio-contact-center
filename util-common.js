c749d886e175c83768947d25017fa08823f952da
bchia <bchia@twilio.com>
2017-02-16 14:15:48 -0800

    working app

--- util-common.js
+++ util-common.js
@@ -1,7 +1,9 @@
 module.exports.generateSessionExirationDate = function (seconds) {
 	// TODO, throw exception is parameter is not set
 	var now = new Date()
-	var offset = (now.getTimezoneOffset() * 60 * 1000) * -1
+	var sideOfGmt = now.getTimezoneOffset() > 0 ? 1 : -1;
+	//var offset = (now.getTimezoneOffset() * 60 * 1000) * -1
+	var offset = (now.getTimezoneOffset() * 60 * 1000) * sideOfGmt
 	var date = new Date(now.getTime() + offset + (seconds * 1000))
 
 	return date
